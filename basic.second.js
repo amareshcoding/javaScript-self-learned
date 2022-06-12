02bc727154f9e4aae6a61fa371628ade9af3c40e
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-06-12 11:50:53 +0530

    datatype

--- basic.second.js
+++ basic.second.js
@@ -1,5 +1,4 @@
-var num1;
-console.log('num1: ', num1);
+
 let num2;
 console.log('num2: ', num2);
 // const num3;
@@ -22,7 +21,21 @@ let bol3 = true/false; //Infinity
 console.log('bol: ', bol1);
 
 //null
+let nullValue = null;
+console.log('nullValue: ', nullValue);
+
+let nullValue1 = "";
+console.log('nullValue1: ', nullValue1);
+
+
 //undefined
+let undefinedValue;
+console.log('undefinedValue: ', undefinedValue);
+
+let undefinedValue1 = undefined;
+console.log('undefinedValue1: ', undefinedValue1);
+
+
 //Infinity
 //
 
@@ -32,6 +45,10 @@ console.log('bol: ', bol1);
 let arr = [4,2,3,1,6,7];
 console.log('arr: ', arr);
 
+let arr1 = new Array(4);
+console.log('arr1: ', arr1);
+
+
 //Object
 let obj = {
      name: "Amaresh",
@@ -39,6 +56,8 @@ let obj = {
 };
 console.log('obj: ', obj);
 
+let obj1 = Object.create(null);
+console.log('obj1: ', obj1);
 
 //Mathematical Operators
 // + -> Add
